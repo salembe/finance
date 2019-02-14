f2269f9c9059798155a919966bb2fa66eb005c81
余友 <yuyou@qutoutiao.net>
2019-02-14 19:20:44 +0800

    delete file

--- __main__.py
+++ __main__.py
@@ -10,9 +10,12 @@
 @time: 2018/9/22 下午4:13
 """
 
+from finance import Finance
+
 
 def main():
-    pass
+    f = Finance()
+    print f.cal_month_cost(10000, 0.023, 12)
 
 
 if __name__ == '__main__':
